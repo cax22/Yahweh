d84427ab259d193da55ae1317d6b2ad20eb8cbd7
billnye22 <caxggg@gmail.com>
2025-03-29 17:40:42 -0400

    Update config for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: process.env.NODE_ENV === "production" ? "/yahweh" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/yahweh/" : "",
 };
 
 module.exports = nextConfig;
